e164dcef5551bbc032148c2f7a86ca2fc66df28a
reyhane javivi <1100.reyhanej@gmail.com>
2021-07-17 12:13:03 +0430

    installed and used socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require("express")
 const app = express()
-const port = process.env.PORT || 3000
+
+const socketIO = require("socket.io")
+const http = require("http")
 const path = require("path")
+
+const server = http.createServer(app)
+
+const port = process.env.PORT || 3000
 
 const publicPath = path.join(__dirname, "../public")
 
+const io = socketIO(server)
+io.on("connection", (socket) => {
+    console.log("New User Connection!")
+
+    socket.on("disconect", () => {
+        console.log("User was Disconnected")
+    })
+})
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`server is running on port ${port}`)
 })
